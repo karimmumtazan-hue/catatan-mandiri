12625e307e187f8526334b5d33664ab5538c888a
some one <karimmumtazan@gmail.com>
2026-08-03 21:10:27 +0700

    first commit

---
+++ day1/jendela&navgsi_fldr.sh
@@ -0,0 +1,23 @@
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal> & 'd:\SMA IT HSI\belajar mandiri\dasar terminal\day1\jendela&navgsi_fldr.sh'
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal> whoami
+desktop-j8bf1ge\user
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal> pwd
+
+Path                                        
+----                                        
+D:\SMA IT HSI\belajar mandiri\dasar terminal
+
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal> 
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal> hostname
+DESKTOP-J8BF1GE
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal> cd Desktop
+cd : Cannot find path 'D:\SMA IT HSI\belajar mandiri\dasar terminal\Desktop' because it does not exist.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (D:\SMA IT HSI\b...erminal\Desktop:String) [Set-Location], ItemNotFoundExc 
+   eption
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal>
