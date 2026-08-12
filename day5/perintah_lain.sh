643ad121f753cb90b7b50f1890584cde8940aa1f
some one <karimmumtazan@gmail.com>
2026-08-12 21:34:16 +0700

    update

---
+++ day5/perintah_lain.sh
@@ -0,0 +1,125 @@
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> history
+
+  Id CommandLine                                                                                                         
+  -- -----------                                                                                                         
+   1 try { . "c:\Users\user\AppData\Local\Programs\Microsoft VS Code\df53daabb1\resources\app\out\vs\workbench\contrib...
+   2 cd day5                                                                                                             
+   3 type perintah_lain.sh                                                                                               
+   4 cat perintah_lain.sh                                                                                                
+   5 type /?                                                                                                             
+   6 cat /?                                                                                                              
+   7 get-help type                                                                                                       
+   8 type -?                                                                                                             
+   9 history                                                                                                             
+  10 clear                                                                                                               
+
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> 
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> history
+
+  Id CommandLine                                                                                                         
+  -- -----------                                                                                                         
+   1 try { . "c:\Users\user\AppData\Local\Programs\Microsoft VS Code\df53daabb1\resources\app\out\vs\workbench\contrib...
+   2 cd day5                                                                                                             
+   3 type perintah_lain.sh                                                                                               
+   4 cat perintah_lain.sh                                                                                                
+   5 type /?                                                                                                             
+   6 cat /?                                                                                                              
+   7 get-help type                                                                                                       
+   8 type -?                                                                                                             
+   9 history                                                                                                             
+  10 clear                                                                                                               
+
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> & type perintah_lain.sh 
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> history
+
+  Id CommandLine                                                                                                         
+  -- -----------                                                                                                         
+   1 try { . "c:\Users\user\AppData\Local\Programs\Microsoft VS Code\df53daabb1\resources\app\out\vs\workbench\contrib...
+   2 cd day5                                                                                                             
+   3 type perintah_lain.sh                                                                                               
+   4 cat perintah_lain.sh                                                                                                
+   5 type /?                                                                                                             
+   6 cat /?                                                                                                              
+   7 get-help type                                                                                                       
+   8 type -?                                                                                                             
+   9 history                                                                                                             
+  10 clear                                                                                                               
+
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> 
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> & type perintah_lain.sh
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> history
+
+  Id CommandLine                                                                                                         
+  -- -----------                                                                                                         
+   1 try { . "c:\Users\user\AppData\Local\Programs\Microsoft VS Code\df53daabb1\resources\app\out\vs\workbench\contrib...
+   2 cd day5                                                                                                             
+   3 type perintah_lain.sh                                                                                               
+   4 cat perintah_lain.sh                                                                                                
+   5 type /?                                                                                                             
+   6 cat /?                                                                                                              
+   7 get-help type                                                                                                       
+   8 type -?                                                                                                             
+   9 history                                                                                                             
+  10 clear                                                                                                               
+
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> 
+
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> help cat
+                                                                                                                         
+NAME
+    Get-Content
+    
+SYNTAX
+    Get-Content [-Path] <string[]> [-ReadCount <long>] [-TotalCount <long>] [-Tail <int>] [-Filter <string>] [-Include 
+    <string[]>] [-Exclude <string[]>] [-Force] [-Credential <pscredential>] [-UseTransaction] [-Delimiter <string>] 
+    [-Wait] [-Raw] [-Encoding {Unknown | String | Unicode | Byte | BigEndianUnicode | UTF8 | UTF7 | UTF32 | Ascii | 
+    Default | Oem | BigEndianUTF32}] [-Stream <string>]  [<CommonParameters>]
+    
+    Get-Content -LiteralPath <string[]> [-ReadCount <long>] [-TotalCount <long>] [-Tail <int>] [-Filter <string>] 
+    [-Include <string[]>] [-Exclude <string[]>] [-Force] [-Credential <pscredential>] [-UseTransaction] [-Delimiter 
+    <string>] [-Wait] [-Raw] [-Encoding {Unknown | String | Unicode | Byte | BigEndianUnicode | UTF8 | UTF7 | UTF32 |    
+    Ascii | Default | Oem | BigEndianUTF32}] [-Stream <string>]  [<CommonParameters>]                                    
+                                                                                                                         
+                                                                                                                         
+ALIASES                                                                                                                  
+    gc                                                                                                                   
+    cat                                                                                                                  
+    type                                                                                                                 
+                                                                                                                         
+                                                                                                                         
+REMARKS                                                                                                                  
+    Get-Help cannot find the Help files for this cmdlet on this computer. It is displaying only partial help.            
+        -- To download and install Help files for the module that includes this cmdlet, use Update-Help.                 
+        -- To view the Help topic for this cmdlet online, type: "Get-Help Get-Content -Online" or                        
+           go to https://go.microsoft.com/fwlink/?LinkID=113310.                                                         
+                                                                                                                         
+                                                                                                                         
+                                                                                                                         
+                                                                                                                         
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> code .
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> history
+>> 
+>>   Id CommandLine                                                                                                         
+>>   -- -----------                                                                                                         
+>>    1 try { . "c:\Users\user\AppData\Local\Programs\Microsoft VS Code\df53daabb1\resources\app\out\vs\workbench\contrib...
+>>    2 cd day5                                                                                                             
+>>    3 type perintah_lain.sh                                                                                               
+>>    4 cat perintah_lain.sh                                                                                                
+>>    5 type /?                                                                                                             
+>>    6 cat /?                                                                                                              
+>>    7 get-help type                                                                                                       
+>>    8 type -?                                                                                                             
+>>    9 history                                                                                                             
+>>   10 clear                                                                                                               
+>> 
+>> 
+>> PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> 
+>> 
+>> 
+PS D:\SMA IT HSI\belajar mandiri\dasar terminal\day5> 
